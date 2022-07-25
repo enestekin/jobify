dae69c99d91545e413794e28b07703eda46f3abd
enestekin <zenestekin@gmail.com>
2022-07-25 11:02:45 +0300

    landing page created

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,29 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            cumque consequatur. Ex velit corrupti, hic repudiandae unde ipsam,
+            ea corporis eius aperiam sint consequuntur, voluptatibus quisquam
+            porro eum maiores quia.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;
