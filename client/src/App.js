9c80d1a39b7fed216df65d8a9deec01bc4a8a1db
enestekin <zenestekin@gmail.com>
2022-08-10 00:57:57 +0300

    pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
-import Landing from './pages/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Dashboard, Register, Landing, Error } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>Dashboard</div>} />
-        <Route path='/register' element={<div>Register</div>} />
+        <Route
+          path='/'
+          element={
+            <div>
+              <Dashboard />
+            </div>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <div>
+              <Register />
+            </div>
+          }
+        />
         <Route path='/landing' element={<Landing />} />
-        <Route path='*' element={<h1>error</h1>} />
+        <Route
+          path='*'
+          element={
+            <h1>
+              <Error />
+            </h1>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
