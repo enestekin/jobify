9faed151c94c657eb15ddf5b7b12ec49b3a2f68c
enestekin <zenestekin@gmail.com>
2022-08-17 01:20:13 +0300

    refactor errors

--- controller/authController.js
+++ controller/authController.js
@@ -1,18 +1,12 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new CustomAPIError('Please provide all values');
+    throw new BadRequestError('Please provide all values');
   }
 
   const user = await User.create({ name, email, password });
