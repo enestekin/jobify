d089f4b4d9f1fe633ac9008fc8b3fbf621309d67
enestekin <zenestekin@gmail.com>
2022-08-21 23:01:46 +0300

    profile page completed

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -88,9 +88,20 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
     <AppContext.Provider
-      value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}
+      value={{
+        ...state,
+        displayAlert,
+        setupUser,
+        toggleSidebar,
+        logoutUser,
+        updateUser,
+      }}
     >
       {children}
     </AppContext.Provider>
