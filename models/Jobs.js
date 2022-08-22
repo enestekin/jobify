8d39cd885230f2d70bc0a8ba2611b0cf8f13c571
enestekin <zenestekin@gmail.com>
2022-08-22 08:33:03 +0300

    job model created

---
+++ models/Jobs.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const JobsSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'internship', 'remote'],
+      default: 'full-time',
+    },
+    jobLocation: {
+      type: String,
+      required: [true, 'Please provide city'],
+      default: 'my city',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Job', JobSchema);
