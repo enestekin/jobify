314268d9ccb92bb428f6843131c3e0370bd17901
enestekin <zenestekin@gmail.com>
2022-08-27 13:18:09 +0300

    link to register page

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 import connectDB from './db/connect.js';
 import authRouter from './routes/authRoutes.js';
 import jobsRouter from './routes/jobsRoutes.js';
@@ -18,14 +22,17 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.get('*', (req, res) => {
+  res.seendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
